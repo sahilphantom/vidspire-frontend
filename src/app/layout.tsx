ce1c56f3d673340b09a15d889cdc26c79ee948ad
sahilphantom <sahilmahmood188@gmail.com>
2025-11-30 00:47:20 +0500

    feat: add @vercel/speed-insights dependency and integrate SpeedInsights component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 // Import the provider
@@ -33,6 +34,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
           {children}
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
