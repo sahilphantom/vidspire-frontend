7a9807a4d69733253b1f829ea2fe1bc240d20d0d
sahilphantom <sahilmahmood188@gmail.com>
2025-12-04 00:56:04 +0500

    feat: add CTASection component to enhance user engagement with a call-to-action

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -2,15 +2,17 @@ import FAQSection from "@/components/home/FAQSection";
 import FeaturesSection from "@/components/home/Features";
 import Hero from "@/components/home/Hero";
 import ProblemSection from "@/components/home/ProblemSection";
+import CTASection from "@/src/components/home/CTASection";
 import SolutionSection from "@/src/components/home/SolutionSection";
 
 export default function Home() {
   return (
     <main className="flex flex-col w-full min-h-screen bg-white dark:bg-black transition-colors duration-300">
       <Hero />
+        <ProblemSection />
+        <SolutionSection />
       <FeaturesSection />
-      <ProblemSection />
-      <SolutionSection />
+      {/* <CTASection /> */}
       <FAQSection />
     </main>
   );
